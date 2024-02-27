013363d4d5d8519db7d0f4fb21e931d2f1eca8c8
Meet5555 <bhimanimeet55555@gmail.com>
2024-02-27 22:59:03 +0530

    added redux-logger middleware

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,7 +2,8 @@ import { applyMiddleware, createStore } from "redux"
 import { rootReducer } from "../reducers/rootReducer"
 import { composeWithDevTools } from "@redux-devtools/extension"
 import {thunk} from "redux-thunk"
+import logger from "redux-logger"
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk,logger)))
 
 export default store
